570276d139080dddd5f2f702a26f9b7f0601ebe7
Bamiwo Adebayo <bammietop@gmail.com>
2024-01-28 16:38:47 +0100

    web flask 6

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application"""
-
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """Remove the current SQLAlchemy Session"""
+def close(self):
+    """ Method to close the session """
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
-def states():
-    data = storage.all(State).values()
-    data_sorted = sorted(data, key=lambda x: x.name)
-    return render_template("9-states.html", states=data_sorted, mode='none')
+def state():
+    """Displays a html page with states"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='all')
 
 
 @app.route('/states/<id>', strict_slashes=False)
-def states_(id):
-    data = storage.all(State).values()
-    data_sorted = sorted(data, key=lambda x: x.name)
-    for state in data_sorted:
-        if id == state.id:
-            return render_template("9-states.html", states=state, mode='id')
-    return render_template("9-states.html", states=data_sorted, mode='invalid')
+def state_by_id(id):
+    """Displays a html page with citys of that state"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
